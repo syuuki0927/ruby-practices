36f84128a8c21c28d421febf423090d9f6adec51
Yuki Sato <yuki.sato@livesense.co.jp>
2025-04-22 19:26:34 +0900

    エラー関連の処理を変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# 引数確認
-raise ArgumentError, 'スコアを入力してください' unless ARGV.length >= 1
+if ARGV.empty?
+  warn('スコアを入力してください')
+  exit false
+end
 
-# 投球ごとのスコアの配列を作成。ストライク（X）は10に
 scores = ARGV[0].split(',').map do |score_str|
   if score_str == 'X'
     10
@@ -12,23 +13,20 @@ scores = ARGV[0].split(',').map do |score_str|
     score_str.to_i
   end
 end
-p scores
 
-# 必要に応じて（ストライクやスペア）後の投球のスコアを参照し、フレームごとの得点を計算していく
 frame_scores = []
-now_i = 0
+throw_cnt = 0
 while frame_scores.length < 10
-  if scores[now_i] == 10
-    frame_scores << scores[now_i..now_i + 2].sum
-    now_i += 1
-  elsif scores[now_i] + scores[now_i + 1] >= 10
-    frame_scores << scores[now_i..now_i + 2].sum
-    now_i += 2
+  if scores[throw_cnt] == 10
+    frame_scores << scores[throw_cnt..throw_cnt + 2].sum
+    throw_cnt += 1
+  elsif scores[throw_cnt] + scores[throw_cnt + 1] >= 10
+    frame_scores << scores[throw_cnt..throw_cnt + 2].sum
+    throw_cnt += 2
   else
-    frame_scores << scores[now_i..now_i + 1].sum
-    now_i += 2
+    frame_scores << scores[throw_cnt..throw_cnt + 1].sum
+    throw_cnt += 2
   end
 end
 
-# 全てのフレームの合計点
 puts frame_scores.sum
