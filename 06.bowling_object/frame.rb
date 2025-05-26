d96c1266247887b51799ee6bcafd1a8bbe3883a6
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-26 11:37:35 +0900

    不要な実装を排除

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,20 +3,17 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :shots, :marks
+  attr_reader :shots
 
   def initialize(first_mark, second_mark = 0, third_mark = nil)
-    @marks = [first_mark, second_mark, third_mark]
-    @shots = @marks.map { |mark| Shot.new(mark) }
+    @shots = [first_mark, second_mark, third_mark].map { |mark| Shot.new(mark) }
 
     # strike?, spare?は@shots[0..1]に依存
     if strike?
       @bonus = @shots[1..2].map(&:score).sum
-      @marks = [@marks[0]]
       @shots = [@shots[0]]
     else
       @bonus = spare? ? @shots[2].score : 0
-      @marks = @marks[0..1]
       @shots = @shots[0..1]
     end
   end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -3,15 +3,8 @@
 require_relative 'frame'
 
 class Game
-  attr_reader :score_marks, :frames
-
   def initialize(score_marks = '')
-    self.score_marks = score_marks
-  end
-
-  def score_marks=(score_marks)
-    @score_marks = score_marks.instance_of?(String) ? score_marks.split(',') : score_marks
-    @frames = create_frames
+    @frames = create_frames(score_marks.split(','))
   end
 
   def score
@@ -20,11 +13,11 @@ class Game
 
   private
 
-  def create_frames
+  def create_frames(score_marks)
     frames = []
     throw_cnt = 0
-    while frames.length < 10 && throw_cnt < @score_marks.length
-      frames << Frame.new(*@score_marks[throw_cnt..throw_cnt + 2])
+    while frames.length < 10
+      frames << Frame.new(*score_marks[throw_cnt..throw_cnt + 2])
       throw_cnt += frames[-1].shots.length
     end
 
