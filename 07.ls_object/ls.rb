32979e4ef3053f45031551b8771cca01a91a6899
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-28 16:39:45 +0900

    long_formatをprivateに設定

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -25,21 +25,6 @@ class Ls
     @col_num = 3
   end
 
-  def long_format
-    @col_num = 1
-    puts "total #{@entries.map(&:block_num).sum}"
-    max_lengths = find_max_length(%i[hardlink_num owner group file_size])
-    @entries.each do |entry|
-      entry.display_string = [entry.accessibility,
-                              entry.hardlink_num.to_s.rjust(max_lengths[:hardlink_num]),
-                              "#{entry.owner.rjust(max_lengths[:owner])} ",
-                              "#{entry.group.rjust(max_lengths[:group])} ",
-                              entry.file_size.to_s.rjust(max_lengths[:file_size]),
-                              entry.datetime_str,
-                              entry.file_name].join(' ')
-    end
-  end
-
   def execute
     long_format if @options['l']
 
@@ -64,6 +49,21 @@ class Ls
   end
 
   private
+
+  def long_format
+    @col_num = 1
+    puts "total #{@entries.map(&:block_num).sum}"
+    max_lengths = find_max_length(%i[hardlink_num owner group file_size])
+    @entries.each do |entry|
+      entry.display_string = [entry.accessibility,
+                              entry.hardlink_num.to_s.rjust(max_lengths[:hardlink_num]),
+                              "#{entry.owner.rjust(max_lengths[:owner])} ",
+                              "#{entry.group.rjust(max_lengths[:group])} ",
+                              entry.file_size.to_s.rjust(max_lengths[:file_size]),
+                              entry.datetime_str,
+                              entry.file_name].join(' ')
+    end
+  end
 
   def find_max_length(keys)
     keys.map do |key|
